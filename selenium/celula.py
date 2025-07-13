46f3f3ab89c0be47db2baa3f03f7dd3e19cbf088
helena-varela <helena.varelakg>
2025-07-13 12:53:26 -0300

    manipulando tabelas

---
+++ selenium/celula.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Tabela Estática
+
+driver = webdriver.Chrome()
+driver.get("https://www.techlistic.com/2017/02/automate-demo-web-table-with-selenium.html")
+celula = driver.find_element(By.XPATH, '//*[@id="customers"]/tbody/tr[2]/td[3]')
+print(f"Conteúdo da Célula: {celula.text}") 
+driver.quit()
---
+++ selenium/dados_tabela.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Exibe a Tabela EStática 
+driver = webdriver.Chrome()
+driver.get("https://www.techlistic.com/2017/02/automate-demo-web-table-with-selenium.html")
+linhas = driver.find_elements(By.XPATH, '//*[@id="customers"]/tbody/tr')
+for linha in linhas:
+    colunas = linha.find_elements(By.TAG_NAME, "td")
+    print([coluna.text for coluna in colunas])
+driver.quit()
